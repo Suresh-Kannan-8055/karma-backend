80ce7eafb206687ed97ad5478903d97741216c01
Suresh-Kannan-8055 <sureshkannangeek@gmail.com>
2024-10-01 08:58:09 +0530

    changes maded

--- src/controller/getcontroller.js
+++ src/controller/getcontroller.js
@@ -18,12 +18,13 @@ const StepData = async (req, res) => {
       [
         {
           type: "card",
+          select:"single",
           data: VehicleTypeData.map((vehicletype) => ({
             vehicle_id: vehicletype.id,
-            vehicle_name: vehicletype.vehicle_name,
-            vehicle_icon: vehicletype.titel,
-            vehicle_color: vehicletype.color,
-            vehicle_border: vehicletype.boder_color,
+            name: vehicletype.vehicle_name,
+            icon: vehicletype.titel,
+            color: vehicletype.color,
+            border_color: vehicletype.boder_color,
             vehicle_type_emission_value: vehicletype.emission_value,
           })),
         },
@@ -39,12 +40,13 @@ const StepData = async (req, res) => {
         },
         {
           type: "card",
+          select:"single",
           data: VehicleFuelTypeData.map((vehiclefueltype) => ({
             uel_type_id: vehiclefueltype.id,
-            fuel_type: vehiclefueltype.fuel_type,
-            fuel_type_icon: vehiclefueltype.titel,
-            fuel_type_color: vehiclefueltype.color,
-            fuel_type_border: vehiclefueltype.boder_color,
+            name: vehiclefueltype.fuel_type,
+            icon: vehiclefueltype.titel,
+            color: vehiclefueltype.color,
+            border_color: vehiclefueltype.boder_color,
             vehicle_fuel_type_emission_value: vehiclefueltype.emission_value,
           })),
         },
@@ -61,12 +63,13 @@ const StepData = async (req, res) => {
       [
         {
           type: "card",
+          select:"single",
           data: FoodItemsData.map((fooditem) => ({
             food_id: fooditem.id,
-            food_type: fooditem.food_type,
-            food_icon: fooditem.titel,
-            food_color: fooditem.color,
-            food_border: fooditem.boder_color,
+            name: fooditem.food_type,
+            icon: fooditem.titel,
+            color: fooditem.color,
+            border_color: fooditem.boder_color,
             food_type_emission_value: fooditem.emission_value,
           })),
         },
@@ -74,12 +77,13 @@ const StepData = async (req, res) => {
       [
         {
           type: "card",
+          select:"multi",
           data: AppliancesData.map((appliance) => ({
             appliance_id: appliance.id,
-            appliance_name: appliance.name,
-            appliance_icon: appliance.title,
-            appliance_color: appliance.color,
-            appliance_border: appliance.boder_color,
+            name: appliance.name,
+            icon: appliance.title,
+            color: appliance.color,
+            border_color: appliance.boder_color,
             appliance_emission_value: appliance.emission_value,
           })),
         },
